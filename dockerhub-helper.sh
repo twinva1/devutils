f59f7e6a4cce5af5b5d5306b552a09db3624651a
Tex Liao <linex1008@gmail.com>
2023-06-17 19:50:46 +0800

    feat: add helper shell scripts

---
+++ dockerhub-helper.sh
@@ -0,0 +1,57 @@
+username=dockermeso
+operation=pull
+
+usage() {
+  echo 'Usage: bash dockerhub-helper.sh -o push/pull | ./dockerhub-helper.sh -o push/pull'
+  exit
+}
+
+push_to_dockerhub() {
+  # Login dockerhub with password-stdin
+  docker login -u $username --password-stdin < password.txt
+
+  for repo in user-service expense-service notification-service ui; do 
+  { 
+    docker tag $repo $username/$repo && docker push $username/$repo
+    docker rmi -f $username/$repo
+  }&
+  done 
+  wait
+}
+
+pull_from_dockerhub() {
+
+  for repo in user-service expense-service notification-service ui; do 
+  { 
+    docker rmi -f $repo
+    docker pull $username/$repo
+    docker tag $username/$repo $repo
+    docker rmi -f $username/$repo
+  }&
+  done 
+  wait
+}
+
+if [ "$#" -gt 2 ]
+then
+  usage
+fi
+
+while getopts o: option
+do
+    case "${option}"
+    in
+    o) operation=${OPTARG};;
+    esac
+done
+
+if [ "$operation" = "pull" ]
+then
+  echo "--- Start pulling images ---"
+  pull_from_dockerhub
+else
+  echo "--- Start pushing images ---"
+  push_to_dockerhub
+fi
+
+echo "--- batch $operation DONE ---"
---
+++ startup.sh
@@ -0,0 +1,12 @@
+rm -rf repos
+
+# Download all repos in parallel
+for repo_name in user-service expense-service notification-service application-web; do 
+{ 
+  git clone git@github.com:twinva1/$repo_name.git repos/$repo_name
+}&
+done 
+wait
+
+# Start All Services
+docker-compose -f docker-compose-basic.yml up -d
